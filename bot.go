a6f6eccd8a2a7821c2a3749a6560f28093861e9c
flw <flw@cpan.org>
2018-06-25 23:56:41 +0800

    Docs: move package comments to doc.go

--- bot.go
+++ bot.go
@@ -1,37 +1,3 @@
-// Package slackbot hopes to ease development of Slack bots by adding helpful
-// methods and a mux-router style interface to the github.com/essentialkaos/slack package.
-//
-// Incoming Slack RTM events are mapped to a handler in the following form:
-// 	bot.Hear("(?i)how are you(.*)").MessageHandler(HowAreYouHandler)
-//
-// The package adds Reply and ReplyWithAttachments methods:
-//	func HowAreYouHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-// 		bot.Reply(evt, "A bit tired. You get it? A bit?", slackbot.WithTyping)
-//	}
-//
-//	func HowAreYouAttachmentsHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-// 		txt := "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
-// 		attachment := slack.Attachment{
-// 			Pretext:   "We bring bots to life. :sunglasses: :thumbsup:",
-// 			Title:     "Host, deploy and share your bot in seconds.",
-// 			TitleLink: "https://beepboophq.com/",
-// 			Text:      txt,
-// 			Fallback:  txt,
-// 			ImageURL:  "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
-// 			Color:     "#7CD197",
-// 		}
-//
-//		attachments := []slack.Attachment{attachment}
-//		bot.ReplyWithAttachments(evt, attachments, slackbot.WithTyping)
-//	}
-//
-// The slackbot package exposes  github.com/essentialkaos/slack RTM and Client objects
-// enabling a consumer to interact with the lower level package directly:
-// 	func HowAreYouHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-// 		bot.RTM.NewOutgoingMessage("Hello", "#random")
-// 	}
-//
-//
 package slackbot
 
 import (
@@ -222,7 +188,7 @@ func (b *Bot) ReplyWithAttachments(evt *slack.MessageEvent, attachments []slack.
 	b.Client.PostMessage(evt.Msg.Channel, "", params)
 }
 
-// ReplyAsThread
+// ReplyInThread
 func (b *Bot) ReplyInThread(evt *slack.MessageEvent, msg string, typing bool) {
 	params := slack.PostMessageParameters{AsUser: true}
 
@@ -262,7 +228,7 @@ func (b *Bot) Type(evt *slack.MessageEvent, msg interface{}) {
 	time.Sleep(sleepDuration)
 }
 
-// BotUserID fetches the botUserID.
+// BotUserID fetches the Bot's user ID.
 func (b *Bot) BotUserID() string {
 	return b.botUserID
 }
